25651f8f29b983ef9719ba3819d18a1902572902
Nikola Heliskova <he.ni@seznam.cz>
2024-02-13 14:52:22 +0100

    Lekce 03 + 04 - vytvoreni tridy Room - vytvoreni getteru a settru - vytvoreni metody pro zadani udaju pokoje

---
+++ src/com/ja/hotel/Room.java
@@ -0,0 +1,73 @@
+package com.ja.hotel;
+
+import java.math.BigDecimal;
+
+public class Room {
+    private int roomNo;
+    private int beds;
+    private boolean withBalcony;
+    private boolean seaView;
+    private BigDecimal price;
+
+//Gettery a settery pro atributy tridy Room
+    public int getRoomNo() {
+        return roomNo;
+    }
+
+    public void setRoomNo(int roomNo) {
+        this.roomNo = roomNo;
+    }
+
+    public int getBeds() {
+        return beds;
+    }
+
+    public void setBeds(int beds) {
+        this.beds = beds;
+    }
+
+    public boolean isWithBalcony() {
+        return withBalcony;
+    }
+
+    public void setWithBalcony(boolean withBalcony) {
+        this.withBalcony = withBalcony;
+    }
+
+    public boolean isSeaView() {
+        return seaView;
+    }
+
+    public void setSeaView(boolean seaView) {
+        this.seaView = seaView;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    //Metoda pro zadani udaju o pokoji
+    public void setRoom(int roomNo,int beds,boolean withBalcony,boolean seaView){
+        this.roomNo = roomNo;
+        this.beds = beds;
+        this.withBalcony = withBalcony;
+        this.seaView = seaView;
+        this.price = price;
+    }
+
+    public Room getRoom(){
+        Room room = new Room();
+        room.setRoomNo(roomNo);
+        room.setBeds(beds);
+        room.setWithBalcony(withBalcony);
+        room.setSeaView(seaView);
+        room.setPrice(price);
+
+        return room;
+    }
+
+}
