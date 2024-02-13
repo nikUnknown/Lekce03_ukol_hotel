fab486dcfb739f689b614eaa62db66b1c505f479
Nikola Heliskova <he.ni@seznam.cz>
2024-02-13 20:36:19 +0100

    Lekce 03 - ukol - hotel - oprava Main - pridani metod pro naplneni a vypis Bookingu

--- src/Main.java
+++ src/Main.java
@@ -5,56 +5,40 @@ import java.util.Date;
 import java.util.List;
 
 import com.ja.hotel.Booking;
-import static com.ja.hotel.BookingManager.addBooking;
+import com.ja.hotel.BookingManager;
 
 public class Main {
+
+
+    static BookingManager bookingManager = new BookingManager();
 
     public static void main(String[] args) {
 
 
         fillBookings();
 
-        //Creating new bookings
-
-        addBooking(3,"Karel","Dvorak",LocalDate.of(1990,5,15),
-                LocalDate.of(2023,6,1),LocalDate.of(2023,6,7),false);
-
-        addBooking(2,"Karel","Dvorak",LocalDate.of(1979,1,3),
-                LocalDate.of(2023,7,18),LocalDate.of(2023,7,21),true);
-
-        System.out.println("");
+        bookingManager.printAllBookings();
 
     }
 
     public static void fillBookings() {
-        addBooking(3,"Karel","Dvorak",LocalDate.of(1990,5,15),
+        bookingManager.addBooking(3,"Karel","Dvorak",LocalDate.of(1990,5,15),
                 LocalDate.of(2023,6,1),LocalDate.of(2023,6,7),false);;
 
 
-        addBooking(2,"Karel","Dvorak",LocalDate.of(1979,1,3),
+        bookingManager.addBooking(2,"Karel","Dvorak",LocalDate.of(1979,1,3),
                 LocalDate.of(2023,7,18),LocalDate.of(2023,7,21),true);
 
 
-        for(int i= 0; i < 20; i=i+2){
-            LocalDate firstCheckIn = LocalDate.of(2024, 8, 1);
-            LocalDate firstCheckOut = LocalDate.of(2024,8,2);
-            LocalDate checkIn = firstCheckIn.plusDays(i);
-            LocalDate checkOut = firstCheckOut.plusDays(i);
+        for(int i= 1; i < 20; i+=2){
+            LocalDate checkIn = LocalDate.of(2024, 8, i);
+            LocalDate checkOut = LocalDate.of(2024,8,i+1);
 
-            addBooking(2,"Karolina","Tmava",LocalDate.of(1987,4,9),
+            bookingManager.addBooking(2,"Karolina","Tmava",LocalDate.of(1987,4,9),
                     checkIn,checkOut,true);
-
-            System.out.println(checkIn+" "+checkOut);
-
-
 
         }
     }
 
-    public static void printAllBookings(){
-
-
-
-    }
 
 }
